179a3f6065d68c96345eedc6fbbb5c43935703de
observerw <mamomobo@live.com>
2020-11-09 16:40:33 +0800

    1

--- frontend/src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-export default ({title}) => {
-
-    return (<div>
-        <h1>{title}</h1>
-    </div>)
-}
--- frontend/src/pages/article.tsx
+++ frontend/src/pages/article.tsx
@@ -1,28 +1,22 @@
-import * as React from 'react';
-import {PageProps, Link, graphql} from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import {useEffect, useState} from "react";
+import * as React from "react"
+import { Layout } from "antd"
+import { CSSProperties } from "react"
 
-interface articleProps {
-    uid: string,
+const style: CSSProperties = {
+  backgroundColor: "ffffff"
 }
 
-const article: React.FC<articleProps> = ({uid}) => {
-    const [content, setContent] = useState('');
-
-    useEffect(() => {
-    }, []);
-
-    return (
-        <Layout>
-            <SEO title='fuck'/>
-            <article>
-                fuck
-            </article>
-        </Layout>
-    )
+const Title: React.SFC<{ title: string }> = ({ title, children }) => {
+  return <div>
+    <h1>{title}</h1>
+    <p>{children}</p>
+  </div>
 }
 
-export default article;
-
+export default () => {
+  return (
+    <div style={style}>
+      <Title title={"114514"} />
+    </div>
+  )
+}
