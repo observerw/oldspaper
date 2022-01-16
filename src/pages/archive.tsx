7106dcb1e638155c95178cc2ed91ba974d8cd028
王博伟 <mamomobo@live.com>
2022-01-16 21:57:36 +0800

    修改bu

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -2,13 +2,11 @@ import PageContainer from "@/components/page-container"
 import React from "react"
 
 export default ({ data }) => {
-    export default () => {
-        return <PageContainer>
-            <div className="center-container h-full flex-col">
-                <p className="text-3xl mt-5 border-b-[10px] border-blue-500/50">
-                    TODO
-                </p>
-            </div>
-        </PageContainer>
-    }
+    return <PageContainer>
+        <div className="center-container h-full flex-col">
+            <p className="text-3xl mt-5 border-b-[10px] border-blue-500/50">
+                TODO
+            </p>
+        </div>
+    </PageContainer>
 }
